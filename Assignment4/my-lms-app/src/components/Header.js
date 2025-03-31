83126ce3d8b622e8ea30ad322f4a8db9a4d5653a
tfozo <abenezermuluneh9@gmail.com>
2025-03-30 23:08:57 -0600

    final release

--- Assignment4/my-lms-app/src/components/Header.js
+++ Assignment4/my-lms-app/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../images/logo.jpg';
-import '../App.css';
+import '../MyStyles.css';
 
 
 const Header = () => {
